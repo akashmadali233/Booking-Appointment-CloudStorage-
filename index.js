a3988c0bf53c155119954469a67eba49ee57dfec
Akash <unocoin@Unocoins-MacBook-Pro.local>
2024-02-03 21:58:40 +0530

    booking appointment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function handleForm(){
+function handleForm(event) {
     event.preventDefault();
 
     const name = document.getElementById("name").value;
@@ -9,33 +9,53 @@ function handleForm(){
         name,
         address,
         phone
-    }
+    };
 
     saveDetails(details)
+        .then(() => {
+            add();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function saveDetails(details) {
+    return axios({
+        method: 'post',
+        url: "https://crudcrud.com/api/66dde0eeb57e46c28cb35dba038bddcc/appointmentData",
+        data: details
+    })
     .then(() => {
-        add(); 
+        console.log("SAVED");
+    })
+    .catch((error) => {
+        console.error(error);
     });
 }
 
-function saveDetails(details){
-    axios({
-        method : 'post',
-        url : "https://crudcrud.com/api/b61eac7b9fb1493cb99c4c315c9cfe57/appointmenetData",
-        data : details
-    })
-    .then((responce)=>{
-        console.log("SAVED");
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+function deletePatientDetails(_id) {
+    axios.delete(`https://crudcrud.com/api/66dde0eeb57e46c28cb35dba038bddcc/appointmentData/${_id}`)
+        .then(() => {
+            console.log("DELETED");
+            add();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function editPatientDetails(_id) {
+
+    axios.get(`https://crudcrud.com/api/66dde0eeb57e46c28cb35dba038bddcc/appointmentData/${_id}`)
+        .then(response=> editPatientDetailsInform(response.data))
+        .catch(err => console.log(err));
+    
 }
 
 function add() {
-    axios.get('https://crudcrud.com/api/b61eac7b9fb1493cb99c4c315c9cfe57/appointmenetData')
+    axios.get('https://crudcrud.com/api/66dde0eeb57e46c28cb35dba038bddcc/appointmentData')
         .then((response) => {
-            console.log(response);
-
             const ul = document.getElementById("unorderlist");
             ul.innerHTML = '';
 
@@ -66,26 +86,60 @@ function add() {
             }
         })
         .catch((error) => {
-            console.log(error);
+            console.error(error);
         });
 }
 
-function deletePatientDetails(_id) {
-    axios.delete(`https://crudcrud.com/api/b61eac7b9fb1493cb99c4c315c9cfe57/appointmenetData/${_id}`)
-        .then((response) => {
-            console.log(response.data);
-            add();
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+ function editPatientDetailsInform(data){
+    if(data){
+        const form = document.createElement('form');
+        form.innerHTML = 
+        `<h3>Booking Appointment</h3>
+        <label for="editedName">Name:</label>
+        <input type="text" id="editedName" value="${data.name}" required>
+        <br>
+        <label for="editedAddress">Address:</label>
+        <input type="text" id="editedAddress" value="${data.address}" required>
+        <br>
+        <label for="editedPhone">Phone:</label>
+        <input type="text" id="editedPhone" value="${data.phone}" required>
+        <br>
+        <button type="button" onclick="saveEdit('${data._id}')">Save</button>
+        <button type="button" onclick="cancelEdit()">Cancel</button>
+        `;
+        const listItem = document.getElementById(`${data._id}`)
+        listItem.innerHTML = ''
+        listItem.appendChild(form);
+
+    }
+ }
+
+ function saveEdit(_id) {
+    const editedName = document.getElementById("editedName").value;
+    const editedAddress = document.getElementById("editedAddress").value;
+    const editedPhone = document.getElementById("editedPhone").value;
+
+    const updatedDetails = {
+        name: editedName,
+        address: editedAddress,
+        phone: editedPhone
+    };
+
+    return axios({
+        method: 'put',
+        url: `https://crudcrud.com/api/66dde0eeb57e46c28cb35dba038bddcc/appointmentData/${_id}`,
+        data: updatedDetails
+    })
+    .then(() => {
+        console.log("UPDATED");
+        window.location.reload();
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 }
 
-function editPatientDetails(_id) {
-    // Implement your edit logic here
-    console.log(`Edit patient with ID: ${_id}`);
+function cancelEdit(){
+    add();
 }
-
 window.onload = add;
-
-
